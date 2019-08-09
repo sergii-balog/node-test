5e27372f704792d5626ffa8f48d7426165b56304
sbalog <sbalog@lanteria.net>
2019-08-09 19:35:01 +0300

    CORS policy added

--- express-demo/middleware/header.js
+++ express-demo/middleware/header.js
@@ -3,6 +3,10 @@ function headerMiddleware(request, response, next) {
   response.removeHeader("X-Powered-By");
   response.header("Access-Control-Allow-Origin", "*");
   response.header("Access-Control-Allow-Headers", "*");
+  response.header(
+    "Access-Control-Allow-Methods",
+    "GET, POST, OPTIONS, PUT, DELETE"
+  );
   next();
 }
 
